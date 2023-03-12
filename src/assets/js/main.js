aa8b1a1440bb09cdcb881b27750e25b0654e1e65
Kilian-Liebich <kilian.liebich@outlook.de>
2023-03-12 13:20:38 +0100

    add loading spinner

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+// LOADING SPINNER //
+
+setTimeout(() => {
+  const loader = document.querySelector('.loader');
+  loader.classList.add('loader-hidden');
+}, 3900);
+
+// END LOADING SPINNER //
