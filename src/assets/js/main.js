455e6b85206b453c4d14ee37fa413b017c6c2cc4
Kilian-Liebich <kilian.liebich@outlook.de>
2023-03-12 16:11:17 +0100

    add overflow hidden to body

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,9 @@
 
 setTimeout(() => {
   const loader = document.querySelector('.loader');
+  const overfowHidder = document.body;
   loader.classList.add('loader-hidden');
+  overfowHidder.classList.remove('overflow');
 }, 3900);
 
 // END LOADING SPINNER //
