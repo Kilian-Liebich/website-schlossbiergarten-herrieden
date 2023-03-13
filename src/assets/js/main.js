0c44d5fd6dc06bcb4f5049b008e8358c98a30aed
Kilian-Liebich <kilian.liebich@outlook.de>
2023-03-13 20:16:57 +0100

    change functionality of nav

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,12 +14,25 @@ setTimeout(() => {
 // OPEN OR CLOSE LIST ITEMS //
 
 const listItems = document.querySelectorAll('.list-wrapper');
+let curListItems = '';
 
 for (let i = 0; i < listItems.length; i++) {
   listItems[i].addEventListener('click', () => {
-    const [, wrapper] = listItems[i].children;
-    wrapper.classList.toggle('close');
-    wrapper.classList.toggle('open');
+    const [elements, wrapper] = listItems[i].children;
+
+    if (curListItems === elements.children[0].innerHTML) {
+      wrapper.classList.toggle('close');
+      wrapper.classList.toggle('open');
+    } else {
+      for (const item of listItems) {
+        item.children[1].classList.add('close');
+        item.children[1].classList.remove('open');
+      }
+      wrapper.classList.toggle('close');
+      wrapper.classList.toggle('open');
+    }
+
+    curListItems = elements.children[0].innerHTML;
   });
 }
 
